35160b18c0b0235f89846a6def13ce75a6736bb2
benjyros <benny.peterhans@gmail.com>
2023-02-23 11:44:20 +0100

    added function for login and registration

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
 import Login from './Login';
 import Registration from './Registration';
-import {auth} from './config';
+
+import { auth } from './config';
+import { onAuthStateChanged } from 'firebase/auth';
 
 function App() {
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        navigation.replace("Home");
-      } else {
-        setEmail("");
-        setPassword("");
-      }
-      return unsubscribe;
-    });
-  }, []);
-
   return (
     <div className='App'>
       <Routes>
+        <Route path='/login' element={<Login />} />
         <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} exact />
         <Route path='/registration' element={<Registration />} />
       </Routes>
     </div>
