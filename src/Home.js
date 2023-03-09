3f549cf9d20e9e598b58d8ef95fdedbd41dbd19f
benjyros <benny.peterhans@gmail.com>
2023-03-09 10:28:28 +0100

    Test Commit

--- src/Home.js
+++ src/Home.js
@@ -22,6 +22,7 @@ export default function Home() {
         
         <section className="bg-gray-50 dark:bg-gray-900">
             <Navbar />
+            Test
             <button type="btn" onClick={handleSignOut} className="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-amber-600 dark:hover:bg-amber-700 dark:focus:ring-amber-800">Abmelden</button>
         </section>
     );
