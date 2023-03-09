a06ed2ce53cbdcd9d7d35168ed42a2a062e96ef5
Benjamin Peterhans <111268946+benjyros@users.noreply.github.com>
2023-03-09 09:48:37 +0100

    Conflicts 2 09.03.2023 (#6)
    * test  3
    
    * added home, login and registration
    
    * added function for login and registration
    
    * finished login and registration
    
    * edited language of registration
    
    * Commits 23.02.23 benjyros (#2)
    
    * test  3
    
    * added home, login and registration
    
    * added function for login and registration
    
    * Conflicts 09.03.2023 (#5)
    
    * test  3
    
    * added home, login and registration
    
    * added function for login and registration
    
    * finished login and registration
    
    * edited language of registration

--- src/Login.js
+++ src/Login.js
@@ -19,7 +19,7 @@ export default function Login() {
             return unsubscribe;
         });
     }, []);
-
+    
     const handleSignIn = (event) => {
         event.preventDefault();
         signInWithEmailAndPassword(auth, email, password)
