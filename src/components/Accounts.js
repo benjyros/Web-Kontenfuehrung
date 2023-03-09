8b41afa16117c0973aae209897da2e050511ded4
benjyros <benny.peterhans@gmail.com>
2023-03-09 10:41:44 +0100

    added components folder with navbar and accounts

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -1,14 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
-import Navbar from './components/Navbar';
-import Accounts from './components/Accounts';
-
-export default function Home() {
+export default function Accounts() {
     return (
         <section className="bg-gray-50 dark:bg-gray-900">
-            <Navbar />
-            <Accounts />
+            Accounts
         </section>
     );
 }
