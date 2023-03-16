0608d5aeb257c00f68f9571c6f3fa39a71e5117d
benjyros <benny.peterhans@gmail.com>
2023-03-16 19:25:55 +0100

    fixes #1; structured home into nav and accounts

---
+++ src/components/Accounts.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+export default function Accounts() {
+    return (
+        <section className="bg-gray-50 dark:bg-gray-900">
+            Accounts
+        </section>
+    );
+}
