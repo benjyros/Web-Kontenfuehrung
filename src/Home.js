311167a732db99ab9e76878d9bfdccff496ceaab
Benjamin Peterhans <111268946+benjyros@users.noreply.github.com>
2023-03-16 19:42:19 +0100

    fixes #11; structured home into nav and accounts (#14)

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React from 'react';
 
-import Navbar from './Navbar';
-import { auth } from './config';
-import { signOut } from 'firebase/auth';
+import Navbar from './components/Navbar';
+import Accounts from './components/Accounts';
 
 export default function Home() {
-    const navigate = useNavigate();
-
-    // Event handler when signing out
-    const handleSignOut = () => {
-        signOut(auth).then(() => {
-            navigate('/login', { replace: true });
-        })
-            .catch((error) => {
-                alert(error.message);
-            })
-    }
-
     return (
-        
         <section className="bg-gray-50 dark:bg-gray-900">
             <Navbar />
-            <button type="btn" onClick={handleSignOut} className="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-amber-600 dark:hover:bg-amber-700 dark:focus:ring-amber-800">Abmelden</button>
+            <Accounts />
         </section>
     );
 }
