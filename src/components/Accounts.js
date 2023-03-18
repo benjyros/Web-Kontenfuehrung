dad0b97f38e8360360a315a8d045c71ffbd82620
benjyros <benny.peterhans@gmail.com>
2023-03-18 22:29:52 +0100

    fixed accounts not showing up

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -7,84 +7,121 @@ import { collection, getDocs, query, where } from "firebase/firestore";
 export default function Accounts() {
     const [accounts, setAccounts] = useState([]);
     const [selected, setSelected] = useState(1);
+    const [loading, setLoading] = useState(true);
 
     const selectedAccount = accounts.find(({ id }) => id === selected);
 
     useEffect(() => {
-        function fetchData() {
-            // Defining all types of accounts
-            var types = ["Privatkonto", "Sparkonto"];
-            // Defining array for all accounts
-            var accounts = [];
-            let count = 0;
-            //Looping through all types of accounts
-            for (let i = 0; i < types.length; i++) {
-                // Query accounts where {types[i]}
-                getDocs(query(collection(firestore, "users", auth.currentUser.uid, "accounts"), where("type", "==", types[i])))
-                    .then((snapshot) => {
-                        //create array of promises
-                        snapshot.forEach((doc) => {
-                            const account = {
-                                account: getData(doc, count)
-                            }
+        async function fetchData() {
+            try {
+                const types = ["Privatkonto", "Sparkonto"];
+                const accounts = [];
 
-                            accounts.push(account);
-                        })
-                    })
+                for (let i = 0; i < types.length; i++) {
+                    const querySnapshot = await getDocs(query(collection(firestore, "users", auth.currentUser.uid, "accounts"), where("type", "==", types[i])));
+
+                    const accountPromises = querySnapshot.docs.map(async (doc) => {
+                        const transactionsRef = collection(firestore, "users", auth.currentUser.uid, "accounts", doc.data().iban, "transactions");
+
+                        const transactionGroups = [];
+
+                        const transactionsSnapshot = await getDocs(transactionsRef);
+
+                        transactionsSnapshot.forEach((transactionDoc) => {
+                            const todaysTransactionsRef = collection(transactionsRef, transactionDoc.data().date, "todaysTransactions");
+
+                            const todaysTransactionsPromise = getDocs(todaysTransactionsRef).then((todaysTransactionsSnapshot) => {
+                                const todaysTransactions = todaysTransactionsSnapshot.docs.map((todaysTransactionDoc) => ({
+                                    type: todaysTransactionDoc.data().type,
+                                    amount: todaysTransactionDoc.data().amount,
+                                    who: todaysTransactionDoc.data().who,
+                                    comment: todaysTransactionDoc.data().comment,
+                                })
+                                );
+
+                                return { date: transactionDoc.data().date, todaysTransactions };
+                            });
+
+                            transactionGroups.push(todaysTransactionsPromise);
+                        });
+
+                        const transactionGroupsResolved = await Promise.all(
+                            transactionGroups
+                        );
+
+                        const newAccount = {
+                            id: accounts.length + 1,
+                            name: doc.data().name,
+                            balance: doc.data().balance + " CHF",
+                            interest: doc.data().interest + "%",
+                            transactions: transactionGroupsResolved,
+                        };
+
+                        return newAccount;
+                    });
+
+                    const accountPromisesResolved = await Promise.all(accountPromises);
+
+                    accounts.push(...accountPromisesResolved);
+                }
+
+                setAccounts(accounts);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setLoading(false);
             }
-            // Set useState with the accounts
-            setAccounts(accounts);
-        };
-        fetchData();
+        }
+
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+            if (user) {
+                fetchData();
+            }
+        });
+        return unsubscribe;
     }, []);
 
-    //function that return promise
-    function getData(doc, accounts) {
-        var todaysTransactions = [];
-        var transactions = [];
-
-        getDocs(collection(firestore, "users", auth.currentUser.uid, "accounts", doc.data().iban, "transactions"))
-            .then((transactionsSnap) => {
-                transactionsSnap.forEach((doc2) => {
-                    getDocs(collection(firestore, "users", auth.currentUser.uid, "accounts", doc.data().iban, "transactions", doc2.data().date, "todaysTransactions"))
-                        .then((todaysTransactionSnap) => {
-                            todaysTransactionSnap.forEach((doc3) => {
-                                const transaction = {
-                                    type: doc3.data().type,
-                                    amount: doc3.data().amount,
-                                    who: doc3.data().who,
-                                    comment: doc3.data().comment
-                                }
-
-                                todaysTransactions.push(transaction);
-                            })
-                            const todaysTransaction = {
-                                date: doc2.data().date,
-                                todaysTransactions: todaysTransactions
-                            }
-                            transactions.push(todaysTransaction);
-
-                            todaysTransactions = [];
-                        })
-                });
-            })
-
-        const newAccount = {
-            id: accounts + 1,
-            name: doc.data().name,
-            balance: doc.data().balance + " CHF",
-            interest: doc.data().interest + "%",
-            transactions: transactions
-        };
-
-        transactions = [];
-
-        return newAccount;
+    if (loading) {
+        return <div>Loading...</div>;
     }
 
     return (
         <section className="bg-gray-50 dark:bg-gray-900">
-            Accounts
+            <table>
+                <thead>
+                    <tr>
+                        <th>Account Name</th>
+                        <th>Balance</th>
+                        <th>Interest</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {accounts.map((account) => (
+                        <tr key={account.id} onClick={() => setSelected(account.id)}>
+                            <td>{account.name}</td>
+                            <td>{account.balance}</td>
+                            <td>{account.interest}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            {selectedAccount && (
+                <div>
+                    <h2>{selectedAccount.name}</h2>
+                    {selectedAccount.transactions.map((transactionGroup) => (
+                        <details key={transactionGroup.date}>
+                            <summary>{transactionGroup.date}</summary>
+                            <ul>
+                                {transactionGroup.todaysTransactions.map((transaction) => (
+                                    <li key={transaction.comment}>
+                                        <strong>{transaction.type}</strong> - {transaction.amount} - {transaction.who} - {transaction.comment}
+                                    </li>
+                                ))}
+                            </ul>
+                        </details>
+                    ))}
+                </div>
+            )}
         </section>
     );
 }
