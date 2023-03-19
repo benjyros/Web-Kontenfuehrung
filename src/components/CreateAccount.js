d86dc9bde1045411fea2c71d10a0fc438b895d8c
Julian Hartmann <69578060+JHartmann-ims@users.noreply.github.com>
2023-03-19 16:18:43 +0100

    designed pages (#49)

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -1,61 +1,90 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import Navbar from './Navbar';
-import createIban from '../functions/iban';
+import Navbar from "./Navbar";
+import createIban from "../functions/iban";
 
 import { auth, firestore } from "../config";
 import { doc, setDoc } from "firebase/firestore";
 
 export default function CreateAccount() {
-    const [name, setName] = useState("");
-    const [interest, setInterest] = useState("");
+  const [name, setName] = useState("");
+  const [interest, setInterest] = useState("");
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    // Create saving account for user
-    const createAccount = (event) => {
-        event.preventDefault();
-        writeIntoDB();
-    }
+  // Create saving account for user
+  const createAccount = (event) => {
+    event.preventDefault();
+    writeIntoDB();
+  };
 
-    const writeIntoDB = () => {
-        const iban = createIban();
-        setDoc(doc(firestore, "users", auth.currentUser.uid, "accounts", iban), {
-            iban: iban,
-            type: "Sparkonto",
-            name: name,
-            interest: interest,
-            balance: 0,
-        });
-        navigate('/', { replace: true });
-    }
+  const writeIntoDB = () => {
+    const iban = createIban();
+    setDoc(doc(firestore, "users", auth.currentUser.uid, "accounts", iban), {
+      iban: iban,
+      type: "Sparkonto",
+      name: name,
+      interest: interest,
+      balance: 0,
+    });
+    navigate("/", { replace: true });
+  };
 
-    return (
-        <section className="bg-gray-50 dark:bg-gray-900">
-            <Navbar />
-            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                    Sparkonto erstellen
-                </h1>
-                <form className="space-y-4 md:space-y-6" onSubmit={createAccount}>
-                    <div>
-                        <input type="text" name="comment" id="comment" onChange={(e) => setName(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Bennenung" required="true" />
-                    </div>
-                    <div>
-                        <div className="form-control">
-                            <label className="input-group">
-                                <input type="number" min="0" max="0.5" step="0.1" placeholder="Zinssatz" onChange={(e) => setInterest(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="true" />
-                                <span>%</span>
-                            </label>
-                        </div>
-                    </div>
-                    <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800">Sparkonto eröffnen</button>
-                    <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                        <Link to='/' className="font-medium text-emerald-600 hover:underline dark:text-emerald-500">Abbrechen</Link>
-                    </p>
-                </form>
+  return (
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <Navbar />
+      <div className="p-6 space-y-4 md:space-y-6 sm:p-8 bg-base-100 flex justify-center">
+        <div className="w-1/2">
+          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white pb-5">
+            Sparkonto erstellen
+          </h1>
+          <form className="space-y-4 md:space-y-6" onSubmit={createAccount}>
+            <div>
+              <input
+                type="text"
+                name="comment"
+                id="comment"
+                onChange={(e) => setName(e.target.value)}
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Bennenung"
+                required="true"
+              />
             </div>
-        </section>
-    );
+            <div>
+              <div className="form-control">
+                <label className="input-group">
+                  <input
+                    type="number"
+                    min="0"
+                    max="0.5"
+                    step="0.1"
+                    placeholder="Zinssatz"
+                    onChange={(e) => setInterest(e.target.value)}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required="true"
+                  />
+                  <span>%</span>
+                </label>
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800"
+            >
+              Sparkonto eröffnen
+            </button>
+            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+              <Link
+                to="/"
+                className="font-medium text-emerald-600 hover:underline dark:text-emerald-500"
+              >
+                Abbrechen
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,130 +1,179 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-import Navbar from './Navbar';
+import Navbar from "./Navbar";
 
-import { auth, firestore} from '../config';
-import { onAuthStateChanged } from 'firebase/auth';
+import { auth, firestore } from "../config";
+import { onAuthStateChanged } from "firebase/auth";
 import { collection, getDocs, query, where } from "firebase/firestore";
 
 export default function Home() {
-    const [accounts, setAccounts] = useState([]);
-    const [selected, setSelected] = useState(0);
-    const [loading, setLoading] = useState(true);
+  const [accounts, setAccounts] = useState([]);
+  const [selected, setSelected] = useState(0);
+  const [loading, setLoading] = useState(true);
 
-    const selectedAccount = accounts.find(({ id }) => id === selected);
+  const selectedAccount = accounts.find(({ id }) => id === selected);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const types = ["Privatkonto", "Sparkonto"];
-                const accounts = [];
-                let count = 0;
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const types = ["Privatkonto", "Sparkonto"];
+        const accounts = [];
+        let count = 0;
 
-                for (let i = 0; i < types.length; i++) {
-                    const querySnapshot = await getDocs(query(collection(firestore, "users", auth.currentUser.uid, "accounts"), where("type", "==", types[i])));
-                    const accountPromises = querySnapshot.docs.map(async (doc) => {
-                        const transactionsRef = collection(firestore, "users", auth.currentUser.uid, "accounts", doc.data().iban, "transactions");
+        for (let i = 0; i < types.length; i++) {
+          const querySnapshot = await getDocs(
+            query(
+              collection(firestore, "users", auth.currentUser.uid, "accounts"),
+              where("type", "==", types[i])
+            )
+          );
+          const accountPromises = querySnapshot.docs.map(async (doc) => {
+            const transactionsRef = collection(
+              firestore,
+              "users",
+              auth.currentUser.uid,
+              "accounts",
+              doc.data().iban,
+              "transactions"
+            );
 
-                        const transactionGroups = [];
+            const transactionGroups = [];
 
-                        const transactionsSnapshot = await getDocs(transactionsRef);
+            const transactionsSnapshot = await getDocs(transactionsRef);
 
-                        transactionsSnapshot.forEach((transactionDoc) => {
-                            const todaysTransactionsRef = collection(transactionsRef, transactionDoc.data().date, "todaysTransactions");
-                            const todaysTransactionsPromise = getDocs(todaysTransactionsRef).then((todaysTransactionsSnapshot) => {
-                                const todaysTransactions = todaysTransactionsSnapshot.docs.map((todaysTransactionDoc) => ({
-                                    type: todaysTransactionDoc.data().type,
-                                    amount: todaysTransactionDoc.data().amount,
-                                    who: todaysTransactionDoc.data().who,
-                                    comment: todaysTransactionDoc.data().comment,
-                                }));
+            transactionsSnapshot.forEach((transactionDoc) => {
+              const todaysTransactionsRef = collection(
+                transactionsRef,
+                transactionDoc.data().date,
+                "todaysTransactions"
+              );
+              const todaysTransactionsPromise = getDocs(
+                todaysTransactionsRef
+              ).then((todaysTransactionsSnapshot) => {
+                const todaysTransactions = todaysTransactionsSnapshot.docs.map(
+                  (todaysTransactionDoc) => ({
+                    type: todaysTransactionDoc.data().type,
+                    amount: todaysTransactionDoc.data().amount,
+                    who: todaysTransactionDoc.data().who,
+                    comment: todaysTransactionDoc.data().comment,
+                  })
+                );
 
-                                return { date: transactionDoc.data().date, todaysTransactions };
-                            });
+                return { date: transactionDoc.data().date, todaysTransactions };
+              });
 
-                            transactionGroups.push(todaysTransactionsPromise);
-                        });
+              transactionGroups.push(todaysTransactionsPromise);
+            });
 
-                        const transactionGroupsResolved = await Promise.all(
-                            transactionGroups
-                        );
+            const transactionGroupsResolved = await Promise.all(
+              transactionGroups
+            );
 
-                        const newAccount = {
-                            id: count++,
-                            iban: doc.data().iban,
-                            name: doc.data().name,
-                            balance: doc.data().balance + " CHF",
-                            interest: doc.data().interest + "%",
-                            transactions: transactionGroupsResolved,
-                        };
+            const newAccount = {
+              id: count++,
+              iban: doc.data().iban,
+              name: doc.data().name,
+              balance: doc.data().balance + " CHF",
+              interest: doc.data().interest + "%",
+              transactions: transactionGroupsResolved,
+            };
 
-                        return newAccount;
-                    });
+            return newAccount;
+          });
 
-                    const accountPromisesResolved = await Promise.all(accountPromises);
+          const accountPromisesResolved = await Promise.all(accountPromises);
 
-                    accounts.push(...accountPromisesResolved);
-                }
-
-                setAccounts(accounts);
-            } catch (error) {
-                console.error(error);
-            } finally {
-                setLoading(false);
-            }
+          accounts.push(...accountPromisesResolved);
         }
 
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            if (user) {
-                fetchData();
-            } else {
-                navigate('/login', { replace: true });
-            }
-        });
-        return unsubscribe;
-    }, []);
-
-    if (loading) {
-        return <div className='flex h-screen items-center justify-center'><h1 className='text-2xl font-bold text-center'>Loading...</h1></div>;
+        setAccounts(accounts);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
     }
 
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        fetchData();
+      } else {
+        navigate("/login", { replace: true });
+      }
+    });
+    return unsubscribe;
+  }, []);
+
+  if (loading) {
     return (
-        <section className="bg-gray-50 dark:bg-gray-900">
-            <Navbar />
-            <div className="flex flex-row overflow-x-auto">
-                {accounts.map((account) => (
-                    <button
-                        key={account.id}
-                        className={`${selected === account.id ? 'bg-blue-500 text-white' : 'bg-gray-200'} p-4 rounded-md mx-2`}
-                        onClick={() => { setSelected(account.id) }}
-                    >
-                        <div className="font-bold">{account.name}</div>
-                        <div>{account.balance}</div>
-                        <div>{account.interest}</div>
-                    </button>
-                ))}
-            </div>
-            {selectedAccount && (
-                <div>
-                    <h2>Transaktionen</h2>
-                    {selectedAccount.transactions.map((transactionGroup) => (
-                        <details key={transactionGroup.date}>
-                            <summary>{transactionGroup.date}</summary>
-                            <ul>
-                                {transactionGroup.todaysTransactions.map((transaction) => (
-                                    <li key={transaction.comment}>
-                                        <strong>{transaction.type}:</strong> {transaction.amount} - {transaction.who}
-                                        <p>Kommentar: {transaction.comment}</p>
-                                    </li>
-                                ))}
-                            </ul>
-                        </details>
-                    ))}
-                </div>
-            )}
-        </section>
+      <div className="flex h-screen items-center justify-center">
+        <h1 className="text-2xl font-bold text-center">Loading...</h1>
+      </div>
     );
+  }
+
+  return (
+    <section className="">
+      <Navbar />
+      <div className="bg-base-100 flex flex-row items-center overflow-x-auto whitespace-nowrap h-[220px] px-5 space-x-10 drop-shadow-md">
+        {accounts.map((account) => (
+          <button
+            key={account.id}
+            className={`${
+              selected === account.id ? "bg-blue-500 text-white" : "bg-gray-200"
+            } p-10 rounded-md mx-2 w-80 flex flex-shrink-0 justify-between`}
+            onClick={() => {
+              setSelected(account.id);
+            }}
+          >
+            <div className="font-bold text-2xl">{account.name}</div>
+            <div>
+              <div>{account.balance}</div>
+              <div>{account.interest}</div>
+            </div>
+          </button>
+        ))}
+      </div>
+
+      {selectedAccount && (
+        <div className="pt-12 bg-neutral h-screen">
+          <h2 className="text-left font-bold text-4xl ml-10">Transaktionen</h2>
+          <div className="w-screen flex justify-center items-center pt-12">
+            <div className="overflow-x-auto w-2/3 mr-80">
+              {selectedAccount.transactions.map((transactionGroup) => (
+                <table className="table w-full">
+                  {/* head */}
+                  <thead>
+                    <tr>
+                      <th>Datum</th>
+                      <th>Bezeichnung</th>
+                      <th>Belastung</th>
+                      <th>Gutschrift</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {transactionGroup.todaysTransactions.map((transaction) => (
+                      <tr>
+                        <th key={transaction.comment}>
+                          {transactionGroup.date}
+                        </th>
+                        <td>
+                          {transaction.type} - {transaction.who}
+                        </td>
+                        <td>{transaction.amount}</td>
+                        <td></td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+    </section>
+  );
 }
